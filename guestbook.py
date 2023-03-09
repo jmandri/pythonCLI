a7c8184897b7170eeb9194b9ec0db24d51c5c76a
jmandri <117689421+jmandri@users.noreply.github.com>
2023-03-09 20:55:17 +0100

    Update guestbook.py

--- guestbook.py
+++ guestbook.py
@@ -1,52 +1,74 @@
-import json
+import sys
+import os
 
-def add_note(guestbook, content):
-    guestbook.append(content)
-    with open("guestbook.txt", "w") as f:
-        f.write(json.dumps(guestbook))
 
-def print_entries(guestbook):
-    for i, entry in enumerate(guestbook):
-        print(f"{i+1}. {entry}")
+def add_new_entry(entry):
+    with open("guestbook.txt", "a") as f:
+        f.write(entry + "\n")
 
-def edit_note(guestbook, index, content):
-    guestbook[index] = content
-    with open("guestbook.txt", "w") as f:
-        f.write(json.dumps(guestbook))
 
-def delete_note(guestbook, index):
-    del guestbook[index]
-    with open("guestbook.txt", "w") as f:
-        f.write(json.dumps(guestbook))
+def list_entries():
+    with open("guestbook.txt", "r") as f:
+        entries = f.readlines()
+        for i, entry in enumerate(reversed(entries)):
+            print(f"{i + 1}. {entry.strip()}")
 
-def export_json(guestbook):
-    print(json.dumps(guestbook))
+
+def edit_entry(num, new_entry):
+    with open("guestbook.txt", "r+") as f:
+        entries = f.readlines()
+        entry_to_edit = entries[-num].strip()
+        f.seek(0)
+        for entry in entries:
+            if entry.strip() == entry_to_edit:
+                f.write(new_entry + "\n")
+            else:
+                f.write(entry)
+        f.truncate()
+
+
+def delete_entry(num):
+    with open("guestbook.txt", "r+") as f:
+        entries = f.readlines()
+        entry_to_delete = entries[-num].strip()
+        f.seek(0)
+        for entry in entries:
+            if entry.strip() != entry_to_delete:
+                f.write(entry)
+        f.truncate()
+
 
 if __name__ == "__main__":
-    try:
-        with open("guestbook.txt", "r") as f:
-            guestbook = json.loads(f.read())
-    except FileNotFoundError:
-        guestbook = []
+    if len(sys.argv) == 1:
+        print("Usage: guestbook.py [new|list|edit|delete] [args]")
+        sys.exit(1)
 
-    if len(guestbook) == 0:
-        print("No entries in guestbook.")
+    command = sys.argv[1]
 
-    while True:
-        action = input("What would you like to do? (add/print/edit/delete/export/quit) ")
-        if action == "add":
-            content = input("Enter your note: ")
-            add_note(guestbook, content)
-        elif action == "print":
-            print_entries(guestbook)
-        elif action == "edit":
-            index = int(input("Enter the index of the entry to edit: ")) - 1
-            content = input("Enter the new content: ")
-            edit_note(guestbook, index, content)
-        elif action == "delete":
-            index = int(input("Enter the index of the entry to delete: ")) - 1
-            delete_note(guestbook, index)
-        elif action == "export":
-            export_json(guestbook)
-        elif action == "quit":
-            break
+    if command == "new":
+        if len(sys.argv) < 3:
+            print("Usage: guestbook.py new [entry]")
+            sys.exit(1)
+        entry = " ".join(sys.argv[2:])
+        add_new_entry(entry)
+        print("Entry added successfully!")
+    elif command == "list":
+        list_entries()
+    elif command == "edit":
+        if len(sys.argv) < 4:
+            print("Usage: guestbook.py edit [num] [new_entry]")
+            sys.exit(1)
+        num = int(sys.argv[2])
+        new_entry = " ".join(sys.argv[3:])
+        edit_entry(num, new_entry)
+        print(f"Entry #{num} edited successfully!")
+    elif command == "delete":
+        if len(sys.argv) < 3:
+            print("Usage: guestbook.py delete [num]")
+            sys.exit(1)
+        num = int(sys.argv[2])
+        delete_entry(num)
+        print(f"Entry #{num} deleted successfully!")
+    else:
+        print("Invalid command")
+        sys.exit(1)
